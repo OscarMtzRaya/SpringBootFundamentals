f0801a844285024a6a66c615cffdff8306d72580
Oscar Martínez <ozcarr753@gmail.com>
2023-08-09 16:47:02 -0700

    Added student repository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,14 +1,19 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.List;
 
 @Component
 public class StudentService {
+    private final StudentRepository _studentRepository;
+    @Autowired
+    public StudentService(StudentRepository _studentRepository) {
+        this._studentRepository = _studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L, "Oscar", "oscar@gmail.com", LocalDate.of(2000, 12, 1), 21)
-        );
+        return _studentRepository.findAll();
     }
 }
